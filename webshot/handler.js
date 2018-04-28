e807e73eacb02c695d563edd0fd2b8b6533d09c0
Rakesh Paul <developer.rakeshpaul@gmail.com>
2018-04-28 12:28:50 +0530

    Working version

--- webshot/handler.js
+++ webshot/handler.js
@@ -1,22 +1,11 @@
 'use strict'
 const puppeteer = require('puppeteer')
 
-module.exports = (context, callback) => {
-  try {
-    const browser = puppeteer.launch({
-      executablePath: '/home/app/headless_shell',
-      headless: true,
-      args: [
-        '--no-sandbox',
-        '--disable-gpu',
-        '--disable-setuid-sandbox',
-        '--single-process',
-        '--headless',
-        `--remote-debugging-port=9222`
-      ]
-    })
-    callback(undefined, { status: 'done' })
-  } catch (error) {
-    callback(error, { status: 'error' })
-  }
+module.exports = async (context, callback) => {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  })
+  const version = await browser.version()
+  browser.close()
+  callback(undefined, { status: 'done', version: version })
 }
